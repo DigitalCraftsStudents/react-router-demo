efda1d1b551df7536150f421a168088a8441431b
Chris Aquino <chris@radishmouse.com>
2021-01-28 16:12:22 -0500

    adds axios and makes ajax calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
 import Home from './components/Home';
 import Stuff from './components/Stuff';
 import About from './components/About';
+
+import axios from 'axios';
 
 import { 
   BrowserRouter as Router,
@@ -10,10 +13,43 @@ import {
 } from 'react-router-dom';
 
 function App() {
+  const [data, setData] = useState({});
+  // The arrow function is what I want to do
+  // The square brackets control when you do it (empty means "on page load")
+  useEffect(() => {
+    axios.get('/api')
+      .then((resp) => {
+        console.log(resp.data);
+        setData(resp.data);
+      });
+  }, []);
+
+  async function getValue() {
+    const resp = await axios.get('/api/counter');
+    setData(resp.data);
+  }
+
+  async function incValue() {
+    const resp = await axios.post('/api/counter');
+    setData(resp.data);
+  }
+
+  async function decValue() {
+    const resp = await axios.put('/api/counter');
+    setData(resp.data);
+  }
+
+  async function resetValue() {
+
+    const resp = await axios.delete('/api/counter');
+    setData(resp.data);
+  }
+
+
   return (
     <Router>
       <div className="App">
-
+      <h1>Here is a message: {data.status}</h1>
       <header>
         <Link to="/">Home</Link>
         <Link to="/stuff">Stuff</Link>
@@ -23,10 +59,17 @@ function App() {
       <Switch>
 
         <Route path="/" exact>
-          <Home />   
+          <Home />
+          <p>data is: {data.value}</p>
+          <button onClick={getValue}>get</button>
+          <button onClick={incValue}>+</button>
+          <button onClick={decValue}>-</button>
+          <button onClick={resetValue}>reset</button>
         </Route>
         <Route path="/stuff">
-          <Stuff />
+          <Stuff 
+            data={data}
+          />
         </Route>
         <Route path="/about">
           <About />
--- src/components/Stuff.jsx
+++ src/components/Stuff.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Stuff(props) {
     return (
+        <>
         <h1>All the stuff goes here</h1>
+        <h2>{props.data.status}</h2>
+        </>
     );
 
 }
